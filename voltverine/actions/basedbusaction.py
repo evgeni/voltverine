1002bde3752e080df2d3c34242ae9d425b0223fb
Evgeni Golov <evgeni@golov.de>
2015-02-25 19:04:52 +0100

    honour DBUS_SYSTEM_BUS_ADDRESS if set in the environment

--- voltverine/actions/basedbusaction.py
+++ voltverine/actions/basedbusaction.py
@@ -1,4 +1,5 @@
 from .baseaction import BaseAction
+import os
 import dbus
 import logging
 logger = logging.getLogger(__name__)
@@ -8,4 +9,7 @@ class BaseDbusAction(BaseAction):
 
     def __init__(self, *args, **kwargs):
         super(BaseDbusAction, self).__init__(args, kwargs)
-        self._system_bus = dbus.SystemBus()
+        if os.environ.get('DBUS_SYSTEM_BUS_ADDRESS'):
+            self._system_bus = dbus.bus.BusConnection(os.environ['DBUS_SYSTEM_BUS_ADDRESS'])
+        else:
+            self._system_bus = dbus.SystemBus()
--- voltverine/plugins/basedbusplugin.py
+++ voltverine/plugins/basedbusplugin.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from .baseplugin import BasePlugin
+import os
 import dbus
 import logging
 logger = logging.getLogger(__name__)
@@ -9,4 +10,7 @@ class BaseDbusPlugin(BasePlugin):
 
     def __init__(self, *args, **kwargs):
         super(BaseDbusPlugin, self).__init__(args, kwargs)
-        self._system_bus = dbus.SystemBus()
+        if os.environ.get('DBUS_SYSTEM_BUS_ADDRESS'):
+            self._system_bus = dbus.bus.BusConnection(os.environ['DBUS_SYSTEM_BUS_ADDRESS'])
+        else:
+            self._system_bus = dbus.SystemBus()
