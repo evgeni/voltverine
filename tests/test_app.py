f4a3afe3d126bb937fd92e750c41e32f39af3777
Evgeni Golov <sargentd@die-welt.net>
2015-03-08 12:00:40 +0100

    more app tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -22,6 +22,7 @@ class TestApp(dbusmock.DBusTestCase):
             self.p_mock.wait()
 
     def test_app(self):
+        (self.p_mock, obj_logind) = self.spawn_server_template('logind', {}, stdout=subprocess.PIPE)
         sys.argv = ['voltverine']
         v = voltverine.app.VoltverineApp()
         output = sys.stdout.getvalue().strip() # because stdout is an StringIO instance
@@ -29,7 +30,20 @@ class TestApp(dbusmock.DBusTestCase):
         self.assertTrue(v._plugins)
         self.assertTrue(v._action.__class__.__name__ == 'LogindPoweroff')
 
+    def test_run(self):
+        (self.p_mock, obj_logind) = self.spawn_server_template('logind', {}, stdout=subprocess.PIPE)
+        sys.argv = ['voltverine']
+        v = voltverine.app.VoltverineApp()
+        v.run()
+
+    def test_verbose(self):
+        (self.p_mock, obj_logind) = self.spawn_server_template('logind', {}, stdout=subprocess.PIPE)
+        sys.argv = ['voltverine', '-v']
+        v = voltverine.app.VoltverineApp()
+        self.assertTrue(v.args.verbose)
+
     def test_help(self):
+        (self.p_mock, obj_logind) = self.spawn_server_template('logind', {}, stdout=subprocess.PIPE)
         with self.assertRaises(SystemExit) as cm:
             sys.argv = ['voltverine', '-h']
             v = voltverine.app.VoltverineApp()
@@ -39,6 +53,7 @@ class TestApp(dbusmock.DBusTestCase):
         self.assertEquals(cm.exception.code, 0)
 
     def test_config_plugins_dict(self):
+        (self.p_mock, obj_logind) = self.spawn_server_template('logind', {}, stdout=subprocess.PIPE)
         with tempfile.NamedTemporaryFile() as cfgf:
             cfgf.write(b"""---
 plugins:
@@ -48,11 +63,13 @@ plugins:
             cfgf.flush()
             sys.argv = ['voltverine', '-c', cfgf.name]
             v = voltverine.app.VoltverineApp()
+            v.run()
             self.assertEquals(len(v._plugins), 1)
             self.assertEquals(v._plugins[0][0], 'NoShutdownFile')
             self.assertEquals(v.config['plugins']['NoShutdownFile']['filename'], '/some/path')
 
     def test_config_plugins_list(self):
+        (self.p_mock, obj_logind) = self.spawn_server_template('logind', {}, stdout=subprocess.PIPE)
         with tempfile.NamedTemporaryFile() as cfgf:
             cfgf.write(b"""---
 plugins:
@@ -62,11 +79,13 @@ plugins:
             cfgf.flush()
             sys.argv = ['voltverine', '-c', cfgf.name]
             v = voltverine.app.VoltverineApp()
+            v.run()
             self.assertEquals(len(v._plugins), 2)
             self.assertTrue('LogindSessions' in v.config['plugins'])
             self.assertTrue('LogindInhibitors' in v.config['plugins'])
 
     def test_config_custom_plugins(self):
+        (self.p_mock, obj_logind) = self.spawn_server_template('logind', {}, stdout=subprocess.PIPE)
         with tempfile.NamedTemporaryFile() as cfgf:
             cfgf.write(b"""---
 plugins:
@@ -75,6 +94,7 @@ plugins:
             cfgf.flush()
             sys.argv = ['voltverine', '-c', cfgf.name]
             v = voltverine.app.VoltverineApp()
+            v.run()
             self.assertEquals(len(v._plugins), 1)
             self.assertTrue('tests.customplugin.CustomPlugin' in v.config['plugins'])
             self.assertEquals(v._plugins[0][1].__name__, 'CustomPlugin')
